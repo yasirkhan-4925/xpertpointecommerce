9fe8f209b3f579f5bd7da5b5d22923cfe0286de0
yasirkhan-4925 <64003803+yasirkhan-4925@users.noreply.github.com>
2021-09-16 20:20:33 +0500

    completed till checkout step component

--- frontend/src/components/CheckoutStep.js
+++ frontend/src/components/CheckoutStep.js
@@ -4,45 +4,50 @@ import {LinkContainer} from 'react-router-bootstrap'
 import { Link } from 'react-router-dom'
  
 
-const CheckoutStep = ({step1, step2, step3 , step4}) => {
+const CheckoutStep = ({step1,step2, step3 ,step4}) => {
        
-
+   
       
     return (
         <>
+
+           
         <Nav className='justify-content-center mb-4'>
               
-            <Nav.Item>
-                {step1  ? (
-                    <LinkContainer to='/login'>  <Nav.Link>Sign In</Nav.Link> </LinkContainer>
-                ) :  <Nav.Link disabled>Sign In</Nav.Link> }
-
-            </Nav.Item>
-
-
-            <Nav.Item>
-                {step2 ? (
-                    <LinkContainer to='/shipping'>  <Nav.Link>Shipping</Nav.Link> </LinkContainer>
-                ) :  <Nav.Link disabled > Shipping</Nav.Link> }
-
-            </Nav.Item>
-
             
-            <Nav.Item>
-                {step3 ? (
-                    <LinkContainer to='/payment'>  <Nav.Link>Payment</Nav.Link> </LinkContainer>
-                ) :  <Nav.Link disabled>Payment</Nav.Link> }
 
-            </Nav.Item>
+                
+                <Nav.Item>
 
-               
-            <Nav.Item>
-                {step4? (
-                    <LinkContainer to='/placeorder'>  <Nav.Link>Place Order</Nav.Link> </LinkContainer>
-                ) :  <Nav.Link disabled>Placer Order</Nav.Link> }
+                    {step1 ?  <Link className='text-light' style={{textDecoration:'none'}} to='/login'> <Nav.Link style={{ color:'black' , fontWeight:'bolder'}}>Sign In</Nav.Link>   </Link>  : <Nav.Link disabled>Sign In</Nav.Link> }
 
-            </Nav.Item>
+                </Nav.Item>
 
+
+                <Nav.Item>
+
+{step2 ?  <Link className='text-light' style={{textDecoration:'none'}} to='/shipping'> <Nav.Link style={{ color:'black' , fontWeight:'bolder'}}>Shipping</Nav.Link>   </Link>  : <Nav.Link disabled>Shipping</Nav.Link> }
+
+</Nav.Item>
+
+                
+
+
+
+<Nav.Item>
+
+{step3 ?  <Link className='text-light' style={{textDecoration:'none'}} to='/payment'> <Nav.Link style={{ color:'black' , fontWeight:'bolder'}}>Payment</Nav.Link>   </Link>  : <Nav.Link disabled>Payment</Nav.Link> }
+
+                </Nav.Item>
+                
+
+                <Nav.Item>
+
+{step4 ?  <Link className='text-light' style={{textDecoration:'none'}} to='/confirmorder'> <Nav.Link style={{ color:'black' , fontWeight:'bolder'}}>Confirm Order</Nav.Link>   </Link>  : <Nav.Link disabled>Confirm Order</Nav.Link> }
+
+</Nav.Item>
+
+          
 
 
             </Nav>
--- frontend/src/components/ShippingScreen.js
+++ frontend/src/components/ShippingScreen.js
@@ -1,133 +1,143 @@
+import React, { useEffect } from 'react';
+import FormContainer from './FormContainer';
+import { Button, Form } from 'react-bootstrap';
 
+import { useDispatch, useSelector } from 'react-redux';
+import CheckoutStep from './CheckoutStep';
 
-import React, { useEffect} from 'react'
-import FormContainer from './FormContainer'
-import {  Button, Form } from 'react-bootstrap'
-
-import { useDispatch, useSelector } from 'react-redux'
-import CheckoutStep from './CheckoutStep'
-
-
-
-import { Formik,  } from 'formik'
-import * as Yup from 'yup'
-import { addShippingAddress } from '../Actions/cartAction'
-
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { addShippingAddress } from '../Actions/cartAction';
 
 const schema = Yup.object({
-    address: Yup.string().required('Address is required'),
-    city: Yup.string().required('city is required'),
-    postalCode: Yup.string().required('postal code is required'),
-    country: Yup.string().required('country is required')
-    
-})
+  address: Yup.string().required('Address is required'),
+  city: Yup.string().required('city is required'),
+  postalCode: Yup.string().required('postal code is required'),
+  country: Yup.string().required('country is required'),
+});
 
+const ShippingScreen = ({ location, history }) => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-const ShippingScreen = ({location , history}) => {
-    
-   
-     
-    const dispatch = useDispatch();
-    const cart = useSelector(state => state.cart);
-    const {shippingAddress} = cart
-      
-    const userLogin = useSelector(state => state.userLogin.user)
+  const userLogin = useSelector((state) => state.userLogin.user);
 
-    useEffect(() => {
-        if (!userLogin) {
-              history.push('/')
-          }
-    },[userLogin , history])
+  useEffect(() => {
+    if (!userLogin) {
+      history.push('/');
+    }
+  }, [userLogin, history]);
 
-    return (
-         
-        
-          
-        <FormContainer>
+  return (
+    <FormContainer>
+      <CheckoutStep step1 step2 />
 
-            <CheckoutStep  />
-         
-              <h1>Shipping</h1>
-            
-            <Formik  validateOnChange={false} validateOnBlur={false} validationSchema={schema} initialValues={{
-                address :shippingAddress.address,
-                city: shippingAddress.city,
-                postalCode:shippingAddress.postalCode,
-                country: shippingAddress.country
-            }} onSubmit={(data, { setSubmitting})=> {
-                 
-             
-                const { address, city, postalCode, country } = data
-                dispatch(addShippingAddress({address, city , postalCode ,country}))
-                history.push('/payment')
-               
-              
-                  
-            }}>
-            
-            {({values,handleChange, handleSubmit  , touched, errors, isSubmitting, isValid}) => {
-                    return (
-                       
-                    <Form noValidate onSubmit={handleSubmit}>
+      <h1>Shipping</h1>
 
-                        <Form.Group controlId='address'>
+      <Formik
+        validateOnChange={false}
+        validateOnBlur={false}
+        validationSchema={schema}
+        initialValues={{
+          address: shippingAddress.address,
+          city: shippingAddress.city,
+          postalCode: shippingAddress.postalCode,
+          country: shippingAddress.country,
+        }}
+        onSubmit={(data, { setSubmitting }) => {
+          const { address, city, postalCode, country } = data;
+          dispatch(addShippingAddress({ address, city, postalCode, country }));
+          history.push('/payment');
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          touched,
+          errors,
+          isSubmitting,
+          isValid,
+        }) => {
+          return (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group controlId='address'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  placeholder='Enter Address'
+                  isInvalid={errors.address}
+                  type='text'
+                  value={values.address}
+                  onChange={handleChange}
+                  name='address'
+                />
 
-                            <Form.Label>Address</Form.Label>
-                            <Form.Control placeholder='Enter Address'  isInvalid={ errors.address } type='text' value={values.address} onChange={ handleChange}  name='address'/>
+                <Form.Control.Feedback type='invalid'>
+                  {errors.address}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-                            <Form.Control.Feedback type='invalid' >{errors.address}</Form.Control.Feedback>
+              <Form.Group controlId='city'>
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  placeholder='Enter City'
+                  isInvalid={errors.city}
+                  type='text'
+                  value={values.city}
+                  onChange={handleChange}
+                  name='city'
+                />
 
-                        </Form.Group>
+                <Form.Control.Feedback type='invalid'>
+                  {errors.city}
+                </Form.Control.Feedback>
+              </Form.Group>
 
+              <Form.Group controlId='postalCode'>
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                  placeholder='Enter Postal Code'
+                  isInvalid={errors.postalCode}
+                  type='text'
+                  value={values.postalCode}
+                  onChange={handleChange}
+                  name='postalCode'
+                />
 
-                         <Form.Group controlId='city'>
+                <Form.Control.Feedback type='invalid'>
+                  {errors.postalCode}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-                            <Form.Label>City</Form.Label>
-                            <Form.Control placeholder='Enter City'  isInvalid={ errors.city} type='text' value={values.city} onChange={ handleChange}  name='city'/>
+              <Form.Group controlId='country'>
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  placeholder='Enter Country'
+                  isInvalid={errors.country}
+                  type='text'
+                  value={values.country}
+                  onChange={handleChange}
+                  name='country'
+                />
 
-                            <Form.Control.Feedback type='invalid' >{errors.city}</Form.Control.Feedback>
-
-                        </Form.Group>
-
-
-                         <Form.Group controlId='postalCode'>
-
-                            <Form.Label>Postal Code</Form.Label>
-                            <Form.Control  placeholder='Enter Postal Code' isInvalid={ errors.postalCode} type='text' value={values.postalCode} onChange={ handleChange}  name='postalCode'/>
-
-                            <Form.Control.Feedback type='invalid' >{errors.postalCode}</Form.Control.Feedback>
-
-                        </Form.Group>
-                          
-                          <Form.Group controlId='country'>
-
-                            <Form.Label>Country</Form.Label>
-                            <Form.Control placeholder='Enter Country' isInvalid={ errors.country} type='text' value={values.country} onChange={ handleChange}  name='country'/>
-
-                            <Form.Control.Feedback type='invalid' >{errors.country}</Form.Control.Feedback>
-
-                        </Form.Group>
-                        <Button  style={{ marginTop: '10px' }} type='submit' variant='primary'>Continue</Button>
-                        
-                        
-                    
-                    </Form>
-                    
-
-                )
-            }}
-            
-
-            </Formik>
-            
-          
-            </FormContainer>
-           
-    )
-
-
-}
-
+                <Form.Control.Feedback type='invalid'>
+                  {errors.country}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Button
+                style={{ marginTop: '10px' }}
+                type='submit'
+                variant='primary'
+              >
+                Continue
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </FormContainer>
+  );
+};
 
 export default ShippingScreen;
