d2d5c2f4fa7ef67dd8c3b3f8f7d544290a5470c6
yasirkhan-4925 <64003803+yasirkhan-4925@users.noreply.github.com>
2021-09-26 17:21:10 +0500

    completed till the back end image upload functionality

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import colors from 'colors';
 import connect from './config/mongoConnect.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import { customErrorHandler , notFind} from './middleware/errorMiddleware.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+import { customErrorHandler, notFind } from './middleware/errorMiddleware.js'
+import path from 'path'
 
 
 import products from './data/products.js';
@@ -28,9 +31,11 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes)
-app.use('/api/orders',orderRoutes)
-
-
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload',uploadRoutes)
+ 
+ const __dirname  = path.resolve()
+app.use('/uploads' , express.static(path.join(__dirname,'/uploads')))
 app.use(notFind);
 app.use(customErrorHandler);
 
