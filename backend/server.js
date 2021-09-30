d2ac9b16b1881ff824dd3d9d8e824cd282d457d5
yasirkhan-4925 <64003803+yasirkhan-4925@users.noreply.github.com>
2021-10-01 00:04:42 +0500

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -55,9 +55,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('api is running....');
-});
 
 
 
@@ -67,10 +64,21 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.use('/api/upload',uploadRoutes)
- 
- const __dirname  = path.resolve()
+app.use('/api/upload', uploadRoutes)
+const __dirname  = path.resolve()
 app.use('/uploads' , express.static(path.join(__dirname,'/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('api is running....');
+  });
+  
+}
+ 
+
 app.use(notFind);
 app.use(customErrorHandler);
 
